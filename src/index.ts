d965d094ba93159d406a5c0491b60c2e8ed1d1b0
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2025-03-06 22:59:12 +0300

    Концепция лоадеров (loaders). Настройка Typescript.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { sum } from "./test"
+
+console.log('Hello World?!')
+console.log(sum(13, 2))
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 module.exports = (env) => {
     return {
         mode: env.mode ?? development,
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: '[name].[contenthash].js',
@@ -14,6 +14,18 @@ module.exports = (env) => {
         plugins: [
             new webpack.ProgressPlugin(),
             new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-          ],
+        ],
+        module: {
+            rules: [
+              {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
+            ],
+          },
+          resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+          },
     }
 }
